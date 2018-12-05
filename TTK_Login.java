1eaffef6021461420db52cdc696720d16f3d6de7
SlLj6vtO <poornima.nallani@gmail.com>
2018-12-05 09:07:15 +0100

    login del

---
+++ TTK_Login.java
@@ -0,0 +1,10 @@
+public class TTK_Login {
+    public boolean Run(String user, String pass) {
+    	if (user.equals("tempUsername") && pass.equals("tempPassword")) {
+        	return true;
+     	
+   	} else {
+        	return false;
+    	}
+	}
+}
