75114d8526a14bee94392c1ddc3d7c7f3f075dcb
JanHol <jan.h.hmg@gmail.com>
2018-12-07 10:30:20 +0100

    added variables loginMethods and createMethods

--- TTK_Main.java
+++ TTK_Main.java
@@ -5,6 +5,8 @@ public class TTK_Main {
   private static String optionSelect = "";
   private static Scanner scan = new Scanner(System.in);
   private static TTK_Users userList = new TTK_Users();
+  private static TTK_Login loginMethods = new TTK_Login();
+  private static TTK_Create createMethods = new TTK_Create();
   private static boolean goLean = true;
   public static final String ANSI_GREEN  = "\u001B[32m";
   public static final String ANSI_RED  = "\u001B[31m";
@@ -27,12 +29,13 @@ public class TTK_Main {
       //if current name is guest that means you are not currently logged in
       System.out.println("please enter your username.");
       String tempUsername = scan.next();
-      if(TTK_Login.checkUsername(tempUsername, userList)) {                     //METODCALL FOR TTK_LOGIN HERE
+      if(loginMethods.checkUsername(tempUsername, userList)) {                     //METODCALL FOR TTK_LOGIN HERE
         //checks that the username exists in our userList and, if so, asks for the password)
         System.out.println("please enter your password.");
         String tempPassword = scan.next();
-        if(TTK_Login.logInUser(tempUsername, tempPassword, userList)) {         //METODCALL FOR TTK_LOGIN HERE
+        if(loginMethods.logInUser(tempUsername, tempPassword, userList)) {         //METODCALL FOR TTK_LOGIN HERE
           //login succeeded, printing welcome message.
+          currentUsername = tempUsername;
           welcomeLine();
           return true;
         } else {
@@ -53,20 +56,25 @@ public class TTK_Main {
   }
 
   public static boolean optionLogout() {
-    currentUsername = "Guest";
-    System.out.println("You are now logged out");
-    welcomeLine();
-    return true;
+    if(currentUsername.equals("Guest")) {
+      System.out.println("You are not currently logged in.");
+      return false;
+    } else {
+      currentUsername = "Guest";
+      System.out.println("You are now logged out");
+      welcomeLine();
+      return true;
+    }
   }
 
   public static boolean optionCreate() {
     System.out.println("Please enter the new username: ");
     String newUsername = scan.next();
-    if(!TTK_Create.checkUsername(newUsername, userList)) {                              //METHOD CALL FOR TTK_Create HERE
+    if(!createMethods.checkUsername(newUsername, userList)) {                              //METHOD CALL FOR TTK_Create HERE
       //checks that the user does not already exists
       System.out.println("Please enter the new user's password");
       String newPassword = scan.next();
-      if(TTK_Create.createUser(newUsername, newPassword, userList)) {                   //METHOD CALL FOR TTK_Create HERE
+      if(createMethods.createUser(newUsername, newPassword, userList)) {                   //METHOD CALL FOR TTK_Create HERE
         System.out.println("New user" + newUsername + "has been created!");
         return true;
       } else {
@@ -88,7 +96,7 @@ public class TTK_Main {
       problemDidNotOccur = optionLogout();
     } else if (optionSelect.contains("create")) {
         problemDidNotOccur = optionCreate();
-    } else if (optionSelect.contains("quit" || optionSelect.contains("exit"))) {
+    } else if (optionSelect.contains("quit") || optionSelect.contains("exit")) {
       goLean = false;
       problemDidNotOccur = true;
     } else {
@@ -105,9 +113,9 @@ public class TTK_Main {
 
   public static void main(String[] args) {
     welcomeLine();
-    optionList();
-
-    System.out.println(optionSelect);
-
+    while (goLean) {
+      optionList();
+      optionHandle();
+    }
   }
 }
