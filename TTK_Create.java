cf11fdf3143698c7a05022359774cfc91228b26b
DerejeAb <derejeabera34@gmail.com>
2018-12-10 07:32:25 +0100

    added TTK_Create

---
+++ TTK_Create.java
@@ -0,0 +1,21 @@
+package loginSystem;
+
+public class TTK_Create {
+
+    Userss users=new Userss();//Array list is created
+
+    public boolean add(String username, String password, TTK_users Userlist  ) {
+        if (!isExist(username)) {
+            users.create(username, password);
+            return true;
+        }
+        return false;
+    }
+
+    public  boolean isExist(String username){
+       return userss.isExist(username);
+
+    }
+}
+
+// is Exist is used as checkUsername and password 
